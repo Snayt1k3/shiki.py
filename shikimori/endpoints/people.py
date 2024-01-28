443c8360f0efd94e8b5bc19046463c9a8ed413ef
Snayt1k3 <snayt1k3twitch@gmail.com>
2024-01-28 12:01:51 +0300

    add people endpoint

---
+++ shikimori/endpoints/people.py
@@ -0,0 +1,86 @@
+import logging
+
+from .base import BaseEndpoint
+from ..exceptions import RequestError
+from ..types.general.photo import Photo
+from ..types.general.people import People, Date, Works, Role
+from ..types.titles.character import Character
+from ..utils.filter import filter_none_parameters
+from ..types.titles.animes import Anime
+from ..types.titles.manga import Manga
+
+
+class PeopleEndpoint(BaseEndpoint):
+    async def ById(self, id: int) -> People | RequestError:
+        response = await self._request.make_request(
+            "GET",
+            url=f"{self._base_url}/api/peoples/{id}",
+            headers=self._headers(),
+        )
+
+        if not isinstance(response, RequestError):
+            return People(
+                **response,
+                birth_on=Date(**response["birth_on"]),
+                deceased_on=Date(**response["deceased_on"]),
+                birthday=Date(**response["birthday"]),
+                image=Photo(**response["image"]),
+                works=[
+                    Works(
+                        anime=Anime(
+                            **work["anime"], image=Photo(**work["anime"]["image"])
+                        )
+                        if work.get("anime")
+                        else None,
+                        manga=Manga(
+                            **work["manga"], image=Photo(**work["manga"]["image"])
+                        )
+                        if work.get("manga")
+                        else None,
+                        role=work["role"],
+                    )
+                    for work in response["works"]
+                ],
+                roles=[
+                    Role(
+                        animes=[
+                            Anime(**anime, image=Photo(**anime["image"]))
+                            for anime in role["animes"]
+                        ],
+                        characters=[
+                            Character(**ch, image=Photo(**ch["image"]))
+                            for ch in role["characters"]
+                        ],
+                    )
+                    for role in response["roles"]
+                ],
+            )
+
+        logging.debug(
+            f"Bad Request(ById): status - {response.status_code}: info - {str(response)}"
+        )
+
+        return response
+
+    async def search(self, id: int, type: str = None) -> list[Character] | RequestError:
+        """
+
+        :param id: number
+        :param type: Must be one of: seyu, mangaka, producer.
+        :return:
+        """
+        response = await self._request.make_request(
+            "GET",
+            url=f"{self._base_url}/api/peoples/{id}",
+            query_params=filter_none_parameters({"type": type}),
+            headers=self._headers(),
+        )
+
+        if not isinstance(response, RequestError):
+            return [Character(**ch, image=Photo(**ch["image"])) for ch in response]
+
+        logging.debug(
+            f"Bad Request(search): status - {response.status_code}: info - {str(response)}"
+        )
+
+        return response
---
+++ shikimori/types/general/people.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass
+from .photo import Photo
+from ..titles.animes import Anime
+from ..titles.manga import Manga
+from ..titles.character import Character
+
+
+@dataclass
+class Date:
+    day: int
+    month: int
+    year: int
+
+
+@dataclass
+class Works:
+    anime: Anime
+    manga: Manga
+    role: str
+
+
+@dataclass
+class Role:
+    animes: list[Anime]
+    characters: list[Character]
+
+
+@dataclass
+class People:
+    id: int
+    name: str
+    russian: str
+    image: Photo
+    url: str
+    japanese: str
+    job_title: str
+    birth_on: Date
+    deceased_on: Date | None
+    website: str
+    groupped_roles: list[str | int]
+    works: list[Works]
+    roles: list[Role]
+    topic_id: int
+    person_favoured: bool
+    producer: bool
+    producer_favoured: bool
+    mangaka: bool
+    mangaka_favoured: bool
+    seyu: bool
+    seyu_favoured: bool
+    updated_at: str
+    thread_id: int
+    birthday: Date
