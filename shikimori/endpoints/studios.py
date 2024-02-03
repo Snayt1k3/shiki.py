e64c12eaa69bf2dc307ee7c594ec764be14322e5
Snayt1k3 <snayt1k3twitch@gmail.com>
2024-02-03 14:46:02 +0300

    add studios

---
+++ shikimori/endpoints/studios.py
@@ -0,0 +1,24 @@
+import logging
+from typing import List
+
+from .base import BaseEndpoint
+from ..exceptions import RequestError
+from ..types.titles.studios import Studio
+
+logger = logging.getLogger(__name__)
+
+
+class StudiosEndpoint(BaseEndpoint):
+    async def list(self) -> List[Studio] | RequestError:
+        response = await self._request.make_request(
+            "GET", url=f"{self._base_url}/api/studios"
+        )
+
+        if not isinstance(response, RequestError):
+            return [Studio(**s) for s in response]
+
+        logger.debug(
+            f"Bad Request(list): status - {response.status_code}: info - {str(response)}"
+        )
+
+        return response
