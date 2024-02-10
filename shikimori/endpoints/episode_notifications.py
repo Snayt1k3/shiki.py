6c3fc2c64ea238b2ce6af79a00249010d02fee19
Snayt1k3 <snayt1k3twitch@gmail.com>
2024-02-10 18:31:58 +0300

    fix endpoint

--- shikimori/endpoints/episode_notifications.py
+++ shikimori/endpoints/episode_notifications.py
@@ -1,33 +1,57 @@
 from .base import BaseEndpoint
-from shikimori.types.general.episode_notification import (
-    EpisodeNotification,
-    EpisodeNotificationResponse,
-)
 from ..exceptions import RequestError
 import logging
-
+from ..utils.filter import filter_none_parameters
+from ..types.general.episode_notification import EpisodeNotification
 
 class EpisodeNotificationEndpoint(BaseEndpoint):
     async def notify(
-        self, episode: EpisodeNotification, token: str
-    ) -> EpisodeNotificationResponse | RequestError:
-        """
-        :param episode: EpisodeNotifications obj
-        :param token: Private token required to access this api
+        self,
+        token: str,
+        anime_id: int,
+        episode: int,
+        aired_at: str,
+        is_fandub: bool = None,
+        is_raw: bool = None,
+        is_subtitles: bool = None,
+        is_anime365: bool = None,
+    ) -> EpisodeNotification | RequestError:
         """
 
+        :param token: Private token required to access this api
+        :param anime_id: Must be a number.
+        :param episode: Must be a number.
+        :param aired_at: Must be a date in iso8601 YYYY-MM-DDThh:mm:ss±hh format
+        :param is_fandub: Must be one of: true, false, 1, 0.
+        :param is_raw: Must be one of: true, false, 1, 0.
+        :param is_subtitles: Must be one of: true, false, 1, 0.
+        :param is_anime365: Must be one of: true, false, 1, 0.
+        """
         response = await self._request.make_request(
             "POST",
             url=f"{self._base_url}/api/v2/episode_notifications",
-            body={"episode_notification": episode.to_dict(), "token": token},
+            body={
+                "episode_notification": filter_none_parameters(
+                    {
+                        "anime_id": anime_id,
+                        "episode": episode,
+                        "aired_at": aired_at,
+                        "is_fandub": is_fandub,
+                        "is_raw": is_raw,
+                        "is_subtitles": is_subtitles,
+                        "is_anime365": is_anime365,
+                    }
+                ),
+                "token": token,
+            },
             headers=self.headers,
         )
 
         if not isinstance(response, RequestError):
-            return EpisodeNotificationResponse(**response)
+            return EpisodeNotification(**response)
 
         logging.debug(
-            f"Bad Request(notify_shikimori_about_anime_episode): status - {response.status_code}: info - {str(response)}"
+            f"Bad Request(notify): status - {response.status_code}: info - {str(response)}"
         )
 
         return response
