08193863691d3d83a4f935b51e4aed4c001b285f
Snayt1k3 <snayt1k3twitch@gmail.com>
2024-02-18 13:33:11 +0300

    add tests for episode_notification

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,3 +12,4 @@ from tests.fixtures.unit.limiter import *
 from tests.fixtures.unit.endpoints.abuse_request import *
 from tests.fixtures.unit.endpoints.topic_ignore import *
 from tests.fixtures.unit.endpoints.user_ignore import *
+from tests.fixtures.unit.endpoints.episode_notification import *
---
+++ tests/fixtures/unit/endpoints/episode_notification.py
@@ -0,0 +1,21 @@
+from shikimori.endpoints.episode_notifications import EpisodeNotificationEndpoint
+import pytest
+
+
+@pytest.fixture
+def episode_notification():
+    return EpisodeNotificationEndpoint("", "", "")
+
+
+@pytest.fixture
+def json_episode_notification():
+    return {
+  "id": 1,
+  "anime_id": 35,
+  "episode": 3,
+  "is_raw": False,
+  "is_subtitles": False,
+  "is_fandub": True,
+  "is_anime365": True,
+  "topic_id": 123
+}
