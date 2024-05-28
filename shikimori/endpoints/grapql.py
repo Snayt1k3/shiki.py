0df686ddd4bd5f85b006271bab16bdf89545c658
Snayt1k3 <snayt1k3twitch@gmail.com>
2024-05-28 10:50:33 +0300

    add methods(BETA)

--- shikimori/endpoints/grapql.py
+++ shikimori/endpoints/grapql.py
@@ -7,6 +7,7 @@ from shikimori.enums import (
     UserRateStatusEnum,
     UserRateOrderInputType,
 )
+from shikimori.exceptions import RequestError
 from .base import BaseEndpoint
 
 logger = logging.getLogger(__name__)
@@ -15,6 +16,7 @@ logger = logging.getLogger(__name__)
 class GraphQlEndpoint(BaseEndpoint):
     async def animes(
         self,
+        query: str,
         page: int = 1,
         limit: int = 1,
         order: OrderEnum = None,
@@ -32,8 +34,10 @@ class GraphQlEndpoint(BaseEndpoint):
         ids: str = None,
         excludeIds: str = None,
         search: str = None,
-    ):
+    ) -> dict | RequestError:
         """
+
+        :param query: graphql query
         :param kind: List of values separated by comma. Add ! before value to apply negative filter. values: tv, movie, ova, ona, special, tv_special, music, pv, cm, tv_13, tv_24, tv_48
         :param status: List of values separated by comma. Add ! before value to apply negative filter.
         :param season: List of values separated by comma. Add ! before value to apply negative filter. Examples: summer_2017, 2016, 2014_2016, 199x
@@ -53,37 +57,172 @@ class GraphQlEndpoint(BaseEndpoint):
         :param limit: Maximum 50
         """
 
-        pass
+        response = await self._request.make_request(
+            "POST",
+            url=f"{self._base_url}/api/graphql",
+            json={
+                "query": query,
+                "variables": {
+                    "page": page,
+                    "limit": limit,
+                    "order": order.value if order else None,
+                    "kind": kind,
+                    "status": status,
+                    "season": season,
+                    "score": score,
+                    "duration": duration,
+                    "rating": rating,
+                    "genre": genre,
+                    "studio": studio,
+                    "franchise": franchise,
+                    "censored": censored,
+                    "mylist": mylist,
+                    "ids": ids,
+                    "excludeIds": excludeIds,
+                    "search": search,
+                },
+            },
+            headers=self.headers,
+        )
+
+        if not isinstance(response, RequestError):
+            return response
+
+        logger.debug(
+            f"Bad Request(animes graphql): status - {response.status_code}: info - {str(response)}"
+        )
+
+        return response
 
     async def characters(
-        self, page: int = 1, limit: int = 1, ids: str = None, search: str = None
-    ):
+        self,
+        query: str,
+        page: int = 1,
+        limit: int = 1,
+        ids: str = None,
+        search: str = None,
+    ) -> dict | RequestError:
         """
 
+        :param query: graphql query
         :param page: int
         :param limit: Maximum 50
         :param ids: list of separated ids
         :param search: str
         """
-        pass
+        response = await self._request.make_request(
+            "POST",
+            url=f"{self._base_url}/api/graphql",
+            json={
+                "query": query,
+                "variables": {
+                    "page": page,
+                    "limit": limit,
+                    "ids": ids,
+                    "search": search,
+                },
+            },
+            headers=self.headers,
+        )
 
-    async def contests(self, page: int = 1, limit: int = 1, ids: str = None):
+        if not isinstance(response, RequestError):
+            return response
+
+        logger.debug(
+            f"Bad Request(characters graphql): status - {response.status_code}: info - {str(response)}"
+        )
+
+        return response
+
+    async def contests(
+        self, query: str, page: int = 1, limit: int = 1, ids: str = None
+    ) -> dict | RequestError:
         """
 
+        :param query: graphql query
         :param page: int
         :param limit: Maximum 10
         :param ids: list of separated ids
         """
-        pass
+        response = await self._request.make_request(
+            "POST",
+            url=f"{self._base_url}/api/graphql",
+            json={
+                "query": query,
+                "variables": {
+                    "page": page,
+                    "limit": limit,
+                    "ids": ids,
+                },
+            },
+            headers=self.headers,
+        )
 
-    async def currentUser(self):
-        pass
+        if not isinstance(response, RequestError):
+            return response
 
-    async def genres(self, entry: GenreEntryTypeEnum):
-        pass
+        logger.debug(
+            f"Bad Request(contests graphql): status - {response.status_code}: info - {str(response)}"
+        )
+
+        return response
+
+    async def currentUser(
+        self,
+        query: str,
+    ) -> dict | RequestError:
+        """
+        :param query: graphql query
+        """
+        response = await self._request.make_request(
+            "POST",
+            url=f"{self._base_url}/api/graphql",
+            json={
+                "query": query,
+            },
+            headers=self.headers,
+        )
+
+        if not isinstance(response, RequestError):
+            return response
+
+        logger.debug(
+            f"Bad Request(currentUser graphql): status - {response.status_code}: info - {str(response)}"
+        )
+
+        return response
+
+    async def genres(
+        self, query: str, entry: GenreEntryTypeEnum
+    ) -> dict | RequestError:
+        """
+        :param query: graphql query
+        :param entry:
+        """
+        response = await self._request.make_request(
+            "POST",
+            url=f"{self._base_url}/api/graphql",
+            json={
+                "query": query,
+                "variables": {
+                    "entryType": str(entry),
+                },
+            },
+            headers=self.headers,
+        )
+
+        if not isinstance(response, RequestError):
+            return response
+
+        logger.debug(
+            f"Bad Request(genres graphql): status - {response.status_code}: info - {str(response)}"
+        )
+
+        return response
 
     async def mangas(
         self,
+        query: str,
         page: int = 1,
         limit: int = 1,
         order: OrderEnum = None,
@@ -99,8 +238,9 @@ class GraphQlEndpoint(BaseEndpoint):
         ids: str = None,
         excludeIds: str = None,
         search: str = None,
-    ):
+    ) -> dict | RequestError:
         """
+        :param query: graphql query
         :param publisher: List of comma separated publisher ids
         :param kind: List of values separated by comma. Add ! before value to apply negative filter. values: tv, movie, ova, ona, special, tv_special, music, pv, cm, tv_13, tv_24, tv_48
         :param status: List of values separated by comma. Add ! before value to apply negative filter.
@@ -117,10 +257,44 @@ class GraphQlEndpoint(BaseEndpoint):
         :param page: must be a number
         :param limit: Maximum 50
         """
-        pass
+        response = await self._request.make_request(
+            "POST",
+            url=f"{self._base_url}/api/graphql",
+            json={
+                "query": query,
+                "variables": {
+                    "page": page,
+                    "limit": limit,
+                    "order": order.value if order else None,
+                    "kind": kind,
+                    "status": status,
+                    "season": season,
+                    "score": score,
+                    "genre": genre,
+                    "franchise": franchise,
+                    "censored": censored,
+                    "mylist": mylist,
+                    "ids": ids,
+                    "excludeIds": excludeIds,
+                    "search": search,
+                    "publisher": publisher,
+                },
+            },
+            headers=self.headers,
+        )
+
+        if not isinstance(response, RequestError):
+            return response
+
+        logger.debug(
+            f"Bad Request(mangas graphql): status - {response.status_code}: info - {str(response)}"
+        )
+
+        return response
 
     async def people(
         self,
+        query: str,
         page: int = 1,
         limit: int = 1,
         ids: str = None,
@@ -128,9 +302,9 @@ class GraphQlEndpoint(BaseEndpoint):
         isSeyu: bool = None,
         isProducer: bool = None,
         isMangaka: bool = None,
-    ):
+    ) -> dict | RequestError:
         """
-
+        :param query: graphql query
         :param page: int
         :param limit: Maximum 10
         :param ids: List of values separated by comma.
@@ -139,19 +313,45 @@ class GraphQlEndpoint(BaseEndpoint):
         :param isProducer: bool
         :param isMangaka: bool
         """
-        pass
+        response = await self._request.make_request(
+            "POST",
+            url=f"{self._base_url}/api/graphql",
+            json={
+                "query": query,
+                "variables": {
+                    "page": page,
+                    "limit": limit,
+                    "search": search,
+                    "isSeyu": isSeyu,
+                    "isProducer": isProducer,
+                    "isMangaka": isMangaka,
+                    "ids": ids,
+                },
+            },
+            headers=self.headers,
+        )
+
+        if not isinstance(response, RequestError):
+            return response
+
+        logger.debug(
+            f"Bad Request(people graphql): status - {response.status_code}: info - {str(response)}"
+        )
+
+        return response
 
     async def userRates(
         self,
+        query: str,
         userId: int = None,
         page: int = 1,
         limit: int = 1,
         targetType: UserRateTargetTypeEnum = None,
         status: UserRateStatusEnum = None,
         order: UserRateOrderInputType = None,
-    ):
+    ) -> dict | RequestError:
         """
-
+        :param query: graphql query
         :param userId: ID of current user is used by default
         :param page: int
         :param limit: maximum - 50
@@ -159,16 +359,69 @@ class GraphQlEndpoint(BaseEndpoint):
         :param status: UserRateStatusEnum
         :param order: UserRateOrderInputType
         """
-        pass
+        response = await self._request.make_request(
+            "POST",
+            url=f"{self._base_url}/api/graphql",
+            json={
+                "query": query,
+                "variables": {
+                    "page": page,
+                    "limit": limit,
+                    "status": status.value if status else None,
+                    "userId": userId,
+                    "targetType": targetType.value if targetType else None,
+                    "order": (
+                        {"field": order.field, "order": order.order} if order else None
+                    ),
+                },
+            },
+            headers=self.headers,
+        )
+
+        if not isinstance(response, RequestError):
+            return response
+
+        logger.debug(
+            f"Bad Request(userRates graphql): status - {response.status_code}: info - {str(response)}"
+        )
+
+        return response
 
     async def users(
-        self, page: int = 1, limit: int = 1, ids: str = None, search: str = None
-    ):
+        self,
+        query: str,
+        page: int = 1,
+        limit: int = 1,
+        ids: str = None,
+        search: str = None,
+    ) -> dict | RequestError:
         """
-
+        :param query: graphql query
         :param page: int
         :param limit: maximum - 50
         :param ids: List of values separated by comma.
         :param search: str
         """
-        pass
+        response = await self._request.make_request(
+            "POST",
+            url=f"{self._base_url}/api/graphql",
+            json={
+                "query": query,
+                "variables": {
+                    "page": page,
+                    "limit": limit,
+                    "search": search,
+                    "ids": ids,
+                },
+            },
+            headers=self.headers,
+        )
+
+        if not isinstance(response, RequestError):
+            return response
+
+        logger.debug(
+            f"Bad Request(users graphql): status - {response.status_code}: info - {str(response)}"
+        )
+
+        return response
